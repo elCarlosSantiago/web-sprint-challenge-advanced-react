f05e5a20f53b877c69d23de7f9a44ec6e12ee32e
CarlosSantiago Nevett <csnevett@gmail.com>
2021-02-12 14:58:07 -0500

    custom hook implemented

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,12 +1,30 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    super()
+    this.state = {
+      plants: [],
+    }
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios
+    .get('http://localhost:3333/plants')
+    .then(res => {
+      this.setState( {
+        plants: res.data.plantsData
+      })
+    })
+    .catch(err => {
+      console.error(err)
+      debugger
+    })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
@@ -26,14 +44,13 @@ export default class PlantList extends Component {
               </div>
               <button
                 className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
+                onClick={() => this.props.addToCart(plant)}>
                 Add to cart
               </button>
             </div>
           </div>
         ))}
       </main>
-    );
+    )
   }
 }
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,23 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react'
+
+const useForm = (initialValues) => {
+  const [values, setValues] = useState(initialValues)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+  const handleChange = (evt) => {
+    setValues({
+      ...values,
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    setShowSuccessMessage(true)
+  }
+
+  return [values, handleChange, handleSubmit, showSuccessMessage]
+}
+
+export default useForm
